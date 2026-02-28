df2b60ed6a7368ed1da7da2c2f530aa50fc0748e
Atik Rahman <ar@arcreativebd.com>
2026-03-01 01:36:50 +0600

    chore: Bump Version To 1.0.1

--- lib/Admin/Controller.php
+++ lib/Admin/Controller.php
@@ -139,7 +139,7 @@ class Controller
     protected function render($template, array $data = [])
     {
         $data['moduleLink'] = $this->moduleLink;
-        $data['version'] = $this->vars['version'] ?? '1.0.0';
+        $data['version'] = $this->vars['version'] ?? '1.0.1';
         $data['csrfToken'] = $this->generateCsrfToken();
         $data['csrfTokenField'] = $this->getCsrfTokenField();
 
